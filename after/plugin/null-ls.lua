52e45641618764d5f25a265dbc8724937715d8cf
Gautier <gautier.edel.info@gmail.com>
2024-12-10 08:35:08 +0100

    init nvim config

---
+++ after/plugin/null-ls.lua
@@ -0,0 +1,17 @@
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.prettier.with({
+            filetypes = { "javascript", "typescript", "css", "html", "json" },
+        }),
+    },
+})
+
+vim.cmd([[
+    augroup FormatOnSave
+        autocmd!
+        autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx lua vim.lsp.buf.format()
+    augroup END
+]])
+
---
+++ after/plugin/telescope.lua
@@ -0,0 +1,20 @@
+local telescope = require('telescope')
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
+
+telescope.setup {
+
+    pickers = {
+
+        find_files = {
+
+            hidden = true
+
+        }
+
+    }
+}

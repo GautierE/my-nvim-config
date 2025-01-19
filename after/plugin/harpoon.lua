81f54cd6ff075f9ebd407ccfb9fd1236df5f5b0c
Gautier <gautier.edel.info@gmail.com>
2025-01-19 16:44:01 +0100

    feat(harpoon): display tabline

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,3 +1,13 @@
+
+vim.cmd("highlight! HarpoonActive guibg=NONE guifg=#8FE2E1")
+vim.cmd("highlight! HarpoonNumberActive guibg=NONE guifg=#8FE2E1")
+require("harpoon").setup({
+    tabline = true,
+    menu = {
+        width = vim.api.nvim_win_get_width(0) - 20,
+    },
+})
+
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 

17f86cefd9e093e675ca743a559707f28dd4e851
Gautier EDEL <gautier@tool4staffing.com>
2025-01-30 17:52:39 +0100

    feat(telescope): grep string searches in hidden file

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,7 +3,10 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    require('telescope.builtin').grep_string({
+        search = vim.fn.input("Grep > "),
+        additional_args = { "--hidden" },
+    })
 end)
 
 telescope.setup {

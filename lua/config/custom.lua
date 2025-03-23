e42cee96812957c3a6a6ba450c1236f75a08382d
Gautier <gautier.edel.info@gmail.com>
2025-03-23 12:34:58 +0100

    feat(lazy): autoupdate on start

--- lua/config/custom.lua
+++ lua/config/custom.lua
@@ -4,3 +4,12 @@ vim.api.nvim_create_autocmd({"BufLeave", "FocusLost"}, {
   command = "silent! wall"
 })
 
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("lazy_autoupdate", { clear = true }),
+  callback = function()
+    if require("lazy.status").has_updates() then
+      require("lazy").update({ show = false })
+    end
+  end,
+})
+
